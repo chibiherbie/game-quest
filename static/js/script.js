193c343479c407cd8dd3f6c278f208d0fcb12e27
Roman <bekkerrdm@gmail.com>
2021-04-08 20:18:45 +0700

    main-info V0.1

--- static/js/script.js
+++ static/js/script.js
@@ -20,9 +20,9 @@ if (animItems.length > 0) {
             if((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
                 animItem.classList.add('_active')
             }
-            // else {
-            //     animItem.classList.remove('_active')
-            // }
+            else {
+                animItem.classList.remove('_active')
+            }
         }
     }
 
