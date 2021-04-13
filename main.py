6f79245e455b2509e371101d44c5d44e885af369
Roman <bekkerrdm@gmail.com>
2021-04-13 20:36:40 +0700

    add time to the site
    -delete some img

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from flask import Flask, render_template
 import os
+import datetime as dt
+import locale
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+locale.setlocale(locale.LC_ALL, "ru")  # задаем локально для вывода даты на русском
 
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    params = {}
+    params['now_date'] = dt.date.today().strftime("%d %B")
+    params['week_date'] = (dt.date.today() + dt.timedelta(weeks=1)).strftime("%d %B")
+    params['days'] = [(dt.date.today() + dt.timedelta(days=i)).strftime('%d %B %A') for i in range(7)]
+
+    return render_template('index.html', params=params)
 
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
+    # app.run(host='0.0.0.0', port=port)
