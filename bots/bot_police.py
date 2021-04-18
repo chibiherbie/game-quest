6e2631b09615542a19461be3f40da9aaf163a2d3
Roman <bekkerrdm@gmail.com>
2021-04-19 00:20:10 +0700

    bot gets a response from api

--- bots/bot_police.py
+++ bots/bot_police.py
@@ -1,9 +1,11 @@
 import logging
+
 import config
 from aiogram import Bot, Dispatcher, executor, types
 import asyncio
 from bots_info.police import police_info
 from random import choice
+from requests import get, post
 
 
 bot = Bot(token=config.TOKEN_POLICE)
@@ -27,5 +29,31 @@ async def said(message: types.Message):
     await bot.send_message(message.chat.id, choice(police_info[-1]))
 
 
+async def get_start():
+    global num
+    while True:
+        bot_p = get(f'http://127.0.0.1:5000/api/bot_police').json()
+        if bot_p['isActive']:
+            num = bot_p['num_block']
+
+            markup = types.ReplyKeyboardMarkup(row_width=1, one_time_keyboard=True)  # создание клавиатуры
+
+            item1 = types.KeyboardButton(police_info[num][0][0])
+            item2 = types.KeyboardButton(police_info[num][1][0])
+            item3 = types.KeyboardButton(police_info[num][2][0])
+            item4 = types.KeyboardButton(police_info[num][3][0])
+
+            markup.add(item1, item2, item3, item4)
+
+            post(f'http://127.0.0.1:5000/api/bot_police', json={
+                "isActive": False,
+                "num_block": num,
+                "text": 'text'})
+
+            await bot.send_message(config.ID_PERSON, bot_p['text'], reply_markup=markup)
+        await asyncio.sleep(1)
+
+
 if __name__ == '__main__':
+    dp.loop.create_task(get_start())
     executor.start_polling(dp, skip_updates=True)
