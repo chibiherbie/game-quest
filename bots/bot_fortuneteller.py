5079d54bdb8de9251dab7afa1986d4e172e94c8d
Roman <bekkerrdm@gmail.com>
2021-04-19 18:21:59 +0700

    add bot_fortuneteller

--- bots/bot_fortuneteller.py
+++ bots/bot_fortuneteller.py
@@ -5,10 +5,10 @@ from requests import get, post
 import logging
 
 import config
-from bots_info.talks import talks_info
+from bots_info.fortuneteller import fortuneteller_info
 
 
-bot = Bot(token=config.TOKEN_TALKS)
+bot = Bot(token=config.TOKEN_FORTUNETELLER)
 dp = Dispatcher(bot)
 
 # переменная для выбора блока из сюжета
@@ -20,35 +20,35 @@ logging.basicConfig(level=logging.INFO)
 @dp.message_handler(content_types=['text'])
 async def said(message: types.Message):
     for i in range(2):
-        if message.text == talks_info[num][i][0]:
+        if message.text == fortuneteller_info[num][i][0]:
             markup_remove = types.ReplyKeyboardRemove()
             await asyncio.sleep(2)
-            await bot.send_message(message.chat.id, talks_info[num][i][1], reply_markup=markup_remove)
+            await bot.send_message(message.chat.id, fortuneteller_info[num][i][1], reply_markup=markup_remove)
 
             post(f'http://127.0.0.1:5000/api/bot_connect', json={
                 "isActive": True,
-                "num_block": talks_info[num][i][2],
+                "num_block": fortuneteller_info[num][i][2],
                 "text": ''})
             return
-    await bot.send_message(message.chat.id, choice(talks_info[-1]))
+    await bot.send_message(message.chat.id, choice(fortuneteller_info[-1]))
 
 
 async def get_start():
     global num
 
     while True:
-        bot_data = get(f'http://127.0.0.1:5000/api/bot_talks').json()
+        bot_data = get(f'http://127.0.0.1:5000/api/bot_fortuneteller').json()
         if bot_data['isActive']:
             num = bot_data['num_block']
 
             markup = types.ReplyKeyboardMarkup(row_width=1, one_time_keyboard=True)  # создание клавиатуры
 
-            item1 = types.KeyboardButton(talks_info[num][0][0])
-            item2 = types.KeyboardButton(talks_info[num][1][0])
+            item1 = types.KeyboardButton(fortuneteller_info[num][0][0])
+            item2 = types.KeyboardButton(fortuneteller_info[num][1][0])
 
             markup.add(item1, item2)
 
-            post(f'http://127.0.0.1:5000/api/bot_talks', json={
+            post(f'http://127.0.0.1:5000/api/bot_fortuneteller', json={
                 "isActive": False,
                 "num_block": num,
                 "text": 'text'})
